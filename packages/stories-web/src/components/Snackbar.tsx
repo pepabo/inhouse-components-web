f38030fbcfcd2b658187c4c81017870816e63595
fumink7 <711fumi@pepabo.com>
2024-04-16 23:15:41 +0900

    Storybookの修正

--- packages/stories-web/src/components/Snackbar.tsx
+++ packages/stories-web/src/components/Snackbar.tsx
@@ -8,22 +8,23 @@ export interface Props extends HTMLProps {
     SemanticColor,
     "neutral" | "positive" | "negative" | "notice"
   >;
+  isActive?: boolean;
+  isInline?: boolean;
 }
 
-export const Snackbar: FC<Props> = ({ color = "neutral" }) => {
+export const Snackbar: FC<Props> = ({
+  color = "neutral",
+  isActive = true,
+  isInline = false,
+}) => {
   return (
     <>
-      <div className={`in-snackbar -color-${color}`}>
+      <div
+        className={`in-snackbar -color-${color} ${isActive ? "--active" : ""} ${
+          isInline ? "-inline" : ""
+        }`}
+      >
         <span className="in-icon" data-icon="check" />
-        <span>アイテム1を削除しました</span>
-        <div className="_trailing">
-          <button className="in-button -size-s -appearance-transparent">
-            <div className="_body">もとに戻す</div>
-          </button>
-        </div>
-      </div>
-      <div className={`in-snackbar -color-${color} -inline`}>
-        <span className="in-icon _icon" data-icon="check" />
         <span>アイテム1を削除しました</span>
         <div className="_trailing">
           <button className="in-button -size-s -appearance-transparent">
--- packages/stories-web/src/snackbar.stories.tsx
+++ packages/stories-web/src/snackbar.stories.tsx
@@ -10,13 +10,25 @@ export default {
 const Template: StoryFn<Props> = (args) => <Snackbar {...args} />;
 
 export const Neutral = Template.bind({});
-Neutral.args = {};
+Neutral.args = { color: "neutral", isActive: true };
 
 export const Positive = Template.bind({});
-Positive.args = { color: "positive" };
+Positive.args = { color: "positive", isActive: true };
 
 export const Negative = Template.bind({});
-Negative.args = { color: "negative" };
+Negative.args = { color: "negative", isActive: true };
 
 export const Notice = Template.bind({});
-Notice.args = { color: "notice" };
+Notice.args = { color: "notice", isActive: true };
+
+export const NeutralInline = Template.bind({});
+NeutralInline.args = { color: "neutral", isActive: true, isInline: true };
+
+export const PositiveInline = Template.bind({});
+PositiveInline.args = { color: "positive", isActive: true, isInline: true };
+
+export const NegativeInline = Template.bind({});
+NegativeInline.args = { color: "negative", isActive: true, isInline: true };
+
+export const NoticeInline = Template.bind({});
+NoticeInline.args = { color: "notice", isActive: true, isInline: true };
